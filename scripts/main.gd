2b98a83e91a26d9f98ccd974240a457019057ae8
TomasTatra <t.vikiszaly@gmail.com>
2026-04-25 17:21:16 +0200

    final functionality code

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,12 +2,17 @@ extends Node3D
 
 @onready var pauseScene: Node = $PauseMenu
 @onready var tutorialScene: Node = $TutorialMenu
+@onready var timer = $Timer
 
-var paused: bool = false
+var pause: bool = false
+var tutorial: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	tutorialScene.hide()
 	pauseScene.hide()
+	create_timer()
+	start_timer()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,10 +24,54 @@ func _input(event):
 		pause_game()
 
 	if event.is_action_pressed("help_action"):
-		tutorialScene.show()
+		show_tutorial()
+
+###
+# timer functionality
+###
+func create_timer():
+	timer.timeout.connect(_on_timer_timeout)
+	timer.wait_time = 3.0
+
+func start_timer():
+	timer.start()
+
+func resume_timer():
+	timer.set_paused(false)
+
+func stop_timer():
+	timer.set_paused(true)
+
+func _on_timer_timeout():
+	timer.stop()
+	game_over("timeout")
+
+###
+# game over functionality
+###
+func game_over(reason):
+	if reason == "timeout":
+		get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
 ###
 # tutorial menu functionality
 ###
+func show_tutorial():
+	if pause:
+		return
+	stop_timer()
+	tutorialScene.show()
+	tutorial = true
+
+func hide_tutorial():
+	if pause:
+		return
+	resume_timer()
+	tutorialScene.hide()
+	tutorial = false
+
 func _on_previous_button_pressed() -> void:
 	pass # Replace with function body.
 	
@@ -30,7 +79,7 @@ func _on_next_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_continue_button_pressed() -> void:
-	tutorialScene.hide()
+	hide_tutorial()
 
 ###
 # pause menu functionality
@@ -39,13 +88,16 @@ func _on_resume_button_pressed() -> void:
 	pause_game()
 
 func _on_exit_button_pressed() -> void:
+	stop_timer()
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
 func pause_game():
-	if paused:
+	if tutorial:
+		return
+	if pause:
 		pauseScene.hide()
-		get_tree().paused = false
+		resume_timer()
 	else:
 		pauseScene.show()
-		get_tree().paused = true
-	paused = !paused
+		stop_timer()
+	pause = !pause
