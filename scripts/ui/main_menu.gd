8819b3f28a1d2cc06c1be08642975b383d091e24
TomasTatra <t.vikiszaly@gmail.com>
2026-04-26 16:06:21 +0200

    Main menu

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,10 +1,5 @@
 extends Control
 
-@onready var buttons_v_box: VBoxContainer = $MarginContainer/VBoxContainer/ButtonsVBox
-
-func _ready() -> void:
-	focus_button()
-
 func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
@@ -13,13 +8,3 @@ func _on_credit_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
-func _on_visbility_changed() -> void:
-	if visible:
-		focus_button()
-
-func focus_button() -> void:
-	if buttons_v_box:
-		var button: Button = buttons_v_box.get_child(0)
-		if button is Button:
-			button.grab_focus()
