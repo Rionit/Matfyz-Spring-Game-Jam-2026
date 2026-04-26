fd8d76a6829e8e3127b733214a6c593a871e60d1
Martin Verner <martin.verner277@student.cuni.cz>
2026-04-26 16:42:27 +0200

    Added helpbook

--- scripts/main.gd
+++ scripts/main.gd
@@ -95,7 +95,6 @@ func load_level(level: int = 1) -> void:
 	# TODO delete comment, only for testing
 	#incoming_folder .add_docs(game_manager.documents_to_submit)
 	start_timer()
-	show_tutorial(tutorial_pages)
 
 func submit_day() -> void:
 	game_manager.evaluate_day()
